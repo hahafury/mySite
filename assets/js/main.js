1dc012472131b575311c250a887f30a75d4feda3
hahafury <bladeb071@gmail.com>
2020-12-25 15:08:39 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ $('a[href*=#]').bind('click', function(e) {
+ e.preventDefault();
+ 
+ var target = $(this).attr("href");
+ 
+ $('html, body').stop().animate({ scrollTop: $(target).offset().top}, 800, function() {
+ location.hash = target;
+ });
+ 
+ return false;
+ });
+});
+
+
---
+++ assets/js/slickSlider.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+    $('.single-item').slick({
+        arrows: true,
+        dots:true,
+        adaptiveHeight: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
